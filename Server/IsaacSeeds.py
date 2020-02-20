0337c0692ddfa43344aec3836728fd234b03d411
Kasper Ravn Breindal <kasperbreindal@gmail.com>
2020-02-20 23:35:43 +0100

    init

---
+++ Server/IsaacSeeds.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask, escape, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    name = request.args.get("name", "World")
+    return f"hello, {escape(name)}!"
+
+
+@app.route("/seed")
+def seed():
+    seed1 = {"seed": "ABCD 1234", "character":"Eden", "gameversion": "Afterbirth Plus", }
+    return render_template("seed.html", title="SeedTest", seed=seed1)
+
+
+if __name__ == "__main__":
+    app.run()
